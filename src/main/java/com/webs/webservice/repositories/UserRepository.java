9d68c4d4109e16f62a376e25f65cddeb79e5f85b
Vitor Rubatino <rubatinovitor@gmail.com>
2022-03-06 17:02:33 -0300

    UserRepository, injecao de dependencia(teste), populando db

---
+++ src/main/java/com/webs/webservice/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.webs.webservice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webs.webservice.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+}
