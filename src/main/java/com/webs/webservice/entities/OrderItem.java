dc29b15ee6ec63b2d0378faba46d64cd6121df5c
Vitor Rubatino <rubatinovitor@gmail.com>
2022-03-08 14:02:54 -0300

    subtotal e total

--- src/main/java/com/webs/webservice/entities/OrderItem.java
+++ src/main/java/com/webs/webservice/entities/OrderItem.java
@@ -67,6 +67,10 @@ public class OrderItem implements Serializable{
 		id.setProdcut(product);
 	}
 	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
