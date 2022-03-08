9efa7ed34e1b5d1ae5439da7f0beb2872dfd1c3d
Vitor Rubatino <rubatinovitor@gmail.com>
2022-03-08 13:54:02 -0300

    Payment one-to-one

--- src/main/java/com/webs/webservice/config/TestConfig.java
+++ src/main/java/com/webs/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.webs.webservice.entities.Category;
 import com.webs.webservice.entities.Order;
 import com.webs.webservice.entities.OrderItem;
+import com.webs.webservice.entities.Payment;
 import com.webs.webservice.entities.Product;
 import com.webs.webservice.entities.User;
 import com.webs.webservice.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 			
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
